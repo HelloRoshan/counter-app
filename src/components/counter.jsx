168aa89a9b9a9aba1c803c5debcd70cb4f542cd8
Roshan <roshanbudhathoki02@gmail.com>
2020-10-10 13:51:03 +0545

    finished Course

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,40 @@
 import React, { Component } from "react";
 class Counter extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps, prevState);
+  }
+
+  componentWillUnmount() {
+    // Do CleanUp for component like timers, listeners to avoid memory leakage
+    console.log('Counter - UnMount');
+  }
+  styles = {
+    marginRight: "1rem",
+    width: "2rem",
+  };
+  returnValue = () => {
+    const { value } = this.props.counter
+    return value === 0 ? "Zero" : value;
+  };
+  getBadgeClasses = () => {
+    let classes = "badge m-3 badge-";
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
+    return classes
+  }
   render() {
-    return <h1>Hello World</h1>;
+    const { counter, onIncrement, onDelete } = this.props;
+    return (
+      <div>
+        <span style={this.styles} className={this.getBadgeClasses()}>{this.returnValue()}</span>
+        <button
+          onClick={() => onIncrement(counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button onClick={() => onDelete(counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+      </div>
+    );
   }
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React  from 'react';
+
+// Stateless Functional Component
+const NavBar = ({ totalCounters }) => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                Navbar&nbsp;
+                <span className="badge badge-pill badge-secondary">
+                    {totalCounters}
+                </span>
+            </a>
+        </nav>
+    )
+}
+export default NavBar;
